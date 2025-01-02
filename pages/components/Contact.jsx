2548227f3afb96d622be368e180f84f34bd58d8e
GNGscr <de.brand808@gmail.com>
2025-01-02 08:55:29 +0200

    move contact to component

---
+++ pages/components/Contact.jsx
@@ -0,0 +1,39 @@
+export default function Contact() {
+    return (
+        <section className="contact" id="contact">
+        <div className="main-contact">
+            <div className="contact-content">
+                <h4>Services</h4>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#menu">Menu</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </div>
+
+            <div className="contact-content">
+                <h4>Delivery</h4>
+                <li><a href="#contact">Uber Eats</a></li>
+                <li><a href="#contact">Wolt</a></li>
+                <li><a href="#contact">10Bis</a></li>
+                <li><a href="#contact">Mishloha</a></li>
+            </div>
+
+            <div className="contact-content">
+                <h4>Contact</h4>
+                <li><a href="#contact">Blog</a></li>
+                <li><a href="#contact">Careers</a></li>
+                <li><a href="#contact">FAQ</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </div>
+
+            <div className="contact-content">
+                <h4>Follow</h4>
+                <li><a href="https://tiktok.com">TikTok</a></li>
+                <li><a href="https://twitter.com">Twitter</a></li>
+                <li><a href="https://facebook.com">Facebook</a></li>
+                <li><a href="https://instagram.com">Instagram</a></li>
+            </div>
+        </div>
+    </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ import shoppingCartOrange from './public/images/shopping-cart-orange.svg';
 import starIconRating from './public/images/star-icon-rating.png';
 import chocolataBall from './public/images/chocolate_ice_cream_scoop.png';
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 
 export default function Home() {
@@ -350,7 +351,8 @@ export default function Home() {
                 </div>
             </section>
   
-            {/* <!-- menu section --> */}
+          {/* <!-- menu section --> */}
+
           <Menu
             data={data}
             typingText={typingText}
@@ -370,41 +372,8 @@ export default function Home() {
             </a>
   
             {/* <!-- contact section --> */}
-            <section className="contact" id="contact">
-                <div className="main-contact">
-                    <div className="contact-content">
-                        <h4>Services</h4>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#menu">Menu</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </div>
-  
-                    <div className="contact-content">
-                        <h4>Delivery</h4>
-                        <li><a href="#contact">Uber Eats</a></li>
-                        <li><a href="#contact">Wolt</a></li>
-                        <li><a href="#contact">10Bis</a></li>
-                        <li><a href="#contact">Mishloha</a></li>
-                    </div>
-  
-                    <div className="contact-content">
-                        <h4>Contact</h4>
-                        <li><a href="#contact">Blog</a></li>
-                        <li><a href="#contact">Careers</a></li>
-                        <li><a href="#contact">FAQ</a></li>
-                        <li><a href="#contact">Contact Us</a></li>
-                    </div>
-  
-                    <div className="contact-content">
-                        <h4>Follow</h4>
-                        <li><a href="https://tiktok.com">TikTok</a></li>
-                        <li><a href="https://twitter.com">Twitter</a></li>
-                        <li><a href="https://facebook.com">Facebook</a></li>
-                        <li><a href="https://instagram.com">Instagram</a></li>
-                    </div>
-                </div>
-            </section>
+
+            <Contact />
   
             <div className="last-text">
                 <p>@ Developed 2024 by Daniel Ehrlich</p>
